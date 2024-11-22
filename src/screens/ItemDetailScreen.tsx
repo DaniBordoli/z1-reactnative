8e51e6ee2bc4bc73f56ee49c519df1d57d320f68
DaniBordoli <d.c.bordoli@gmail.com>
2024-11-22 04:54:28 -0300

    añadido ItemDetailScreen.tsx y sus respectivos estilos, añadido PlayerScreen.tsx provisional.

--- src/screens/ItemDetailScreen.tsx
+++ src/screens/ItemDetailScreen.tsx
@@ -1,13 +1,40 @@
-import React, { useState } from 'react';
-import {Title } from '../styles/ItemListScreen.styles';
+import React from 'react';
+import { RouteProp, useRoute } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
+import { Container, Image, Title, Author, Content, StyledButton, ButtonText } from '../styles/ItemDetailScreen.styles';
+
+type ItemDetailScreenRouteProp = RouteProp<
+  { ItemDetailScreen: { itemId: string; title: string; image: string; author: string; content: string } },
+  'ItemDetailScreen'
+>;
+
 const ItemDetailScreen = () => {
-   
-  
-    return (
-      
-        <Title>provisional text</Title>
-      
-    );
-  };
-  
-  export default ItemDetailScreen;
+  const route = useRoute<ItemDetailScreenRouteProp>();
+  const { title, image, author, content } = route.params;
+  const navigation = useNavigation();
+  const provisionalImage = 'https://images.pexels.com/photos/1563356/pexels-photo-1563356.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1';
+
+
+  return (
+    <Container>
+      <Title>{title}</Title>
+      <Author>{author}</Author>
+      {image && (
+        <Image
+          source={{ uri: provisionalImage }}
+          resizeMode="cover"
+        />
+      )}
+      <Content>{content}</Content>
+      <StyledButton
+        onPress={() =>
+          navigation.navigate('PlayerScreen', { title, author })
+        }
+      >
+        <ButtonText>Abrir Reproductor</ButtonText>
+      </StyledButton>
+    </Container>
+  );
+};
+
+export default ItemDetailScreen;
---
+++ src/styles/ItemDetailScreen.styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.ScrollView`
+  flex-grow: 1;
+  padding: 20px;
+  background-color: #320e50;
+`;
+
+export const Title = styled.Text`
+  font-size: 28px;
+  font-weight: bold;
+  margin-bottom: 8px;
+  color: white;
+`;
+
+export const Author = styled.Text`
+  font-size: 16px;
+  font-style: italic;
+  margin-bottom: 20px;
+  color: #d1b3e0;
+`;
+
+export const Image = styled.Image`
+  width: 100%;
+  height: 220px;
+  border-radius: 10px;
+  margin-bottom: 20px;
+`;
+
+export const Content = styled.Text`
+  font-size: 16px;
+  line-height: 24px;
+  color: white;
+  margin-bottom: 30px;
+  
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  background-color: #4c1c78; 
+  padding: 15px;
+  border-radius: 8px;
+  align-items: center;
+  margin-top: 20px;
+  margin-bottom: 30px; 
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+`;
